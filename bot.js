0b29aa58619fa35d443a3e6e60bd24f56095a5fd
Amir Hossein Kargar <amirkaargar@hotmail.com>
2025-06-12 01:24:22 +0330

    refactor: change contentType to x-www-form-urlencoded

--- bot.js
+++ bot.js
@@ -1,19 +1,21 @@
-require("dotenv").config()
-const TelegramBot = require("node-telegram-bot-api")
-const axios = require('axios')
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
 
-const TOKEN = process.env.BOT_TOKEN
-const VPN_API_KEY = process.env.VPN_API_KEY
+const TOKEN = process.env.BOT_TOKEN;
+const VPN_API_KEY = process.env.VPN_API_KEY;
 
-const bot = new TelegramBot(TOKEN, {polling: true})
+const bot = new TelegramBot(TOKEN, { polling: true });
 
 bot.onText(/\/create_test/, async (msg) => {
   const chatId = msg.chat.id;
 
   try {
+    const formData = new URLSearchParams();
+    formData.append("test", 1);
     const res = await axios.post(
       "https://robot.wizardxray.shop/bot/api/v1/create",
-      JSON.stringify({ test: 1 }),
+      formData,
       {
         headers: {
           Authorization: `Bearer ${VPN_API_KEY}`,
@@ -25,11 +27,17 @@ bot.onText(/\/create_test/, async (msg) => {
     const data = res.data;
 
     if (data.ok) {
-      bot.sendMessage(chatId, `✅ سرویس تست ساخته شد!\n🔗 لینک: ${data.result.link}`);
+      bot.sendMessage(
+        chatId,
+        `✅ سرویس تست ساخته شد!\n🔗 لینک: ${data.result.link}`
+      );
     } else {
       bot.sendMessage(chatId, `❌ خطا: ${data.error}`);
     }
   } catch (err) {
-    bot.sendMessage(chatId, `⚠️ خطای سرور:\n${err.response?.data?.error || err.message}`);
+    bot.sendMessage(
+      chatId,
+      `⚠️ خطای سرور:\n${err.response?.data?.error || err.message}`
+    );
   }
-}); 
+});
