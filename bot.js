7c7c945f12b1c78b6506978b311f72bf192d6b7b
Amir Hossein Kargar <amirkaargar@hotmail.com>
2025-06-12 00:26:28 +0330

    first commit

---
+++ bot.js
@@ -0,0 +1,34 @@
+const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
+
+const TOKEN = process.env.BOT_TOKEN;
+const VPN_API_KEY = process.env.VPN_API_KEY 
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.onText(/\/create_test/, async (msg) => {
+  const chatId = msg.chat.id;
+
+  try {
+    const res = await axios.post(
+      "https://robot.wizardxray.shop/bot/api/v1/create",
+      { test: 1 },
+      {
+        headers: {
+          Authorization: `Bearer ${VPN_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = res.data;
+
+    if (data.ok) {
+      bot.sendMessage(chatId, `✅ سرویس تست با موفقیت ساخته شد.\n\n🔗 لینک: ${data.result.link}`);
+    } else {
+      bot.sendMessage(chatId, `❌ خطا: ${data.error || "مشکلی پیش آمده"}`);
+    }
+  } catch (err) {
+    bot.sendMessage(chatId, `⚠️ خطای سرور:\n${err.response?.data?.error || err.message}`);
+  }
+});
