8beffee1a075d4009ef1a1b5976f3c79108b6d74
Amir Hossein Kargar <amirkaargar@hotmail.com>
2025-06-12 13:40:14 +0330

    feat:

--- bot.js
+++ bot.js
@@ -1,3 +1,11 @@
+const createTest = require("./createTest");
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
+
+const TOKEN = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
 
