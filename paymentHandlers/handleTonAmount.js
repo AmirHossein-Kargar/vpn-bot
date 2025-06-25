141a645dd779f69a8b6e3cd3f97de994185c7b91
Amir Hossein Kargar <amirkaargar@hotmail.com>
2025-06-26 00:32:10 +0330

    fix: removes user's wrong input message for cleaner chat

--- paymentHandlers/handleTonAmount.js
+++ paymentHandlers/handleTonAmount.js
@@ -5,12 +5,30 @@ module.exports = async function handleTonAmount(bot, msg) {
 
   const { valid, amount, message } = validationAmount(text, 50000, 5000000);
 
+  bot.deleteMessage();
+
   if (!valid) {
-    return bot.sendMessage(chatId, message);
+    return bot.editMessageText(message, {
+      chat_id: chatId,
+      message_id: msg.message_id,
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "🔙 بازگشت به روش‌ های پرداخت",
+              callback_data: "back_to_topup",
+            },
+          ],
+        ],
+      },
+    });
   }
 
-  await bot.sendMessage(
-    chatId,
-    `✅ مبلغ ${amount.toLocaleString()} تومان ثبت شد و فاکتور در حال ساخت است.`
+  await bot.editMessageText(
+    `✅ مبلغ ${amount.toLocaleString()} تومان ثبت شد و فاکتور در حال ساخت است.`,
+    {
+      chat_id: chatId,
+      message_id: msg.message_id,
+    }
   );
-}; 
+};
