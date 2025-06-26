2328a82d3ca293702ea500132b8e78ce9cceea3c
Amir Hossein Kargar <amirkaargar@hotmail.com>
2025-06-27 00:57:26 +0330

    perf: Migrate to official redis package to handle session

---
+++ redisClient.js
@@ -0,0 +1,16 @@
+import { createClient } from "redis";
+
+const client = createClient({
+  userName: "amirkargar",
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: "redis-14166.c328.europe-west3-1.gce.redns.redis-cloud.com",
+    port: 14166,
+  },
+});
+
+client.on("error", (err) => console.error("❌ Redis Error:", err));
+
+await client.connect();
+
+export default client;
---
+++ sessionStore.js
@@ -0,0 +1,16 @@
+import redis from "./redisClient";
+
+const prefix = "session:";
+
+export async function getSession(chatId) {
+  const data = await redis.get(prefix + chatId);
+  return data ? JSON.parse(data) : null;
+}
+
+export async function setSession(chatId, value) {
+  await redis.set(prefix + chatId, JSON.stringify(value));
+}
+
+export async function deleteSession(chatId) {
+  await redis.del(prefix + chatId);
+}
