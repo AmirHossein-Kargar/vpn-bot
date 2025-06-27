11c209d7f303a46b986a6fe3d607bc8248987fca
Amir Hossein Kargar <amirkaargar@hotmail.com>
2025-06-28 00:44:25 +0330

    fix: changed sessionStore.js to commonjs

--- sessionStore.js
+++ sessionStore.js
@@ -1,16 +1,22 @@
-const redis = require("./redisClient")
+const redis = require("./redisClient");
 
 const prefix = "session:";
 
-export async function getSession(chatId) {
+async function getSession(chatId) {
   const data = await redis.get(prefix + chatId);
   return data ? JSON.parse(data) : null;
 }
 
-export async function setSession(chatId, value) {
+async function setSession(chatId, value) {
   await redis.set(prefix + chatId, JSON.stringify(value));
 }
 
-export async function deleteSession(chatId) {
+async function deleteSession(chatId) {
   await redis.del(prefix + chatId);
 }
+
+module.exports = {
+  getSession,
+  setSession,
+  deleteSession,
+};
