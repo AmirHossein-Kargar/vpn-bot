045df10622e3722626723b60dec8763da83ea3dc
Amir Hossein Kargar <amirkaargar@hotmail.com>
2025-06-28 02:24:42 +0330

    refactor: refactor Redis client initialization and sessionStore to use async getRedisClient function

--- redisClient.js
+++ redisClient.js
@@ -1,18 +1,26 @@
 const { createClient } = require("redis");
 
-const client = createClient({
-  username: "amirkargar",
-  password: process.env.REDIS_PASSWORD,
-  socket: {
-    host: "redis-14166.c328.europe-west3-1.gce.redns.redis-cloud.com",
-    port: 14166,
-  },
-});
+let client;
 
-client.on("error", (err) => console.error("❌ Redis Error:", err));
+async function getRedisClient() {
+  if (!client) {
+    client = createClient({
+      username: process.env.REDIS_USERNAME,
+      password: process.env.REDIS_PASSWORD,
+      socket: {
+        host: process.env.REDIS_HOST,
+        port: 14166,
+      },
+    });
+  }
+  client.on("error", (err) => console.error("❌ Redis Error:", err));
 
-client.connect()
-  .then(() => console.log("✅ Redis connected"))
-  .catch(console.error);
-  
-module.exports = client;
+  await client
+    .connect()
+    .then(() => console.log("✅ Redis connected"))
+    .catch(console.error);
+
+  return client;
+}
+
+module.exports = getRedisClient;
--- sessionStore.js
+++ sessionStore.js
@@ -1,18 +1,21 @@
-const redis = require("./redisClient");
+const getRedisClient = require("./redisClient")
 
 const prefix = "session:";
 
 async function getSession(chatId) {
-  const data = await redis.get(prefix + chatId);
+  const client = await getRedisClient()
+  const data = await client.get(prefix + chatId);
   return data ? JSON.parse(data) : null;
 }
 
 async function setSession(chatId, value) {
-  await redis.set(prefix + chatId, JSON.stringify(value));
+  const client = await getRedisClient()
+  await client.set(prefix + chatId, JSON.stringify(value));
 }
 
 async function deleteSession(chatId) {
-  await redis.del(prefix + chatId);
+  const client = await getRedisClient()
+  await client.del(prefix + chatId);
 }
 
 module.exports = {
