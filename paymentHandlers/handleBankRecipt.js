76e6e51a48135c536d78851c7d3073706c8797a9
Amir Hossein Kargar <amirkaargar@hotmail.com>
2025-07-28 17:22:35 +0330

    refactor: recipt sent to gp

--- paymentHandlers/handleBankRecipt.js
+++ paymentHandlers/handleBankRecipt.js
@@ -8,8 +8,7 @@ const handleBankRecipt = async (bot, msg, session) => {
   const user = msg.from;
   const groupId = process.env.GROUP_ID;
 
-  const phoneNumber =
-    (await User.findOne({ telegramId: user.id })?.phoneNumber) || "نامشخص";
+
 
   // * 1. Find the invoice by paymentId and update status to "waiting_for_approval"
   if (session.paymentId) {
@@ -23,23 +22,30 @@ const handleBankRecipt = async (bot, msg, session) => {
     }
   }
 
-  const RLM = "\u200F"; // Right-to-Left Mark
+  // Fetch phoneNumber from DB
+  const dbUser = await User.findOne({ telegramId: user.id });
+  const phoneNumber = dbUser && dbUser.phoneNumber ? dbUser.phoneNumber : "نامشخص";
+  // Remove +98 from phoneNumber if present
+  const displayPhoneNumber = phoneNumber.startsWith("+98")
+    ? phoneNumber.replace("+98", "0")
+    : phoneNumber;
 
-  await bot.sendPhoto(groupId, fileId, {
-    caption:
-      `🧾 <b>رسید جدید پرداخت</b>\n\n` +
-      `👤 <b>نام کاربر:</b> <code>${user.first_name || "نامشخص"}</code>\n` +
-      ` <b>آیدی عددی:</b> <code>${RLM}${user.id}</code>\n` +
-      `📎 <b>یوزرنیم:</b> @${user.username || "ندارد"}\n` +
-      `📞 <b>شماره تلفن:</b> <code>${phoneNumber}</code>\n` +
-      `💰 <b>مبلغ:</b> <code>${session.rawAmount.toLocaleString(
-        "en-US"
-      )} تومان</code>\n` +
-      `📌 <b>شماره فاکتور:</b> <code>${
-        session.paymentId ? RLM + session.paymentId : "نامشخص"
-      }</code>`,
-    parse_mode: "HTML",
-  });
+    const RLM = "\u200F";       // Right-To-Left Mark
+    const LTR = "\u202A";       // Left-To-Right Embedding
+    const PDF = "\u202C";       // Pop Directional Formatting
+    
+    await bot.sendPhoto(groupId, fileId, {
+      caption:
+        `🧾 <b>رسید جدید پرداخت</b>\n\n` +
+        `👤 <b>نام کاربر:</b> <code>${user.first_name || "نامشخص"}</code>\n` +
+        `<b>آیدی عددی:</b> <code>${LTR}${user.id}${PDF}</code>\n` +
+        `📎 <b>یوزرنیم:</b> @${user.username || "ندارد"}\n` +
+        `📞 <b>شماره تلفن:</b> <code>${displayPhoneNumber}</code>\n` +
+        `💰 <b>مبلغ:</b> <code>${session.rawAmount.toLocaleString("en-US")} تومان</code>\n` +
+        `📌 <b>شماره فاکتور:</b> <code>${session.paymentId ? `${LTR}${session.paymentId}${PDF}` : "نامشخص"}</code>`,
+      parse_mode: "HTML",
+    });
+    
 
   // * 3. Delete the user's photo message
   await bot.deleteMessage(chatId, msg.message_id);
